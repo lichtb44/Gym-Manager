e1e7f8019e9b0cd8f0e9f5e1c7c6314246c36d57
lichtb44 <lichtb44@email.com>
2026-05-15 02:28:11 +0800

    Pre-Final Commit 6

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,8 +31,7 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/favicon.svg?v=fitcore" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         @fonts
